8e49a7e0a0b9101b51926bbb576c3a5396bad31a
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-12-02 00:34:17 +0600

    Basic Project Setup Done

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ['dist/'], // Ignore specific directories or files
+    extends: [
+      'eslint:recommended', // ESLint's recommended rules
+      'plugin:@typescript-eslint/recommended', // TypeScript recommended rules
+      'plugin:prettier/recommended', // Add Prettier's recommended rules
+    ],
+  },
+
+  {
+    languageOptions: {
+      globals: globals.browser, // Define global variables
+    },
+
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+  },
+  pluginJs.configs.recommended, // Use recommended JS settings
+  ...tseslint.configs.recommended, // Use recommended TS settings
+];
